7f82aec559d9555e34e470d91c0f201b9cfaf60b
Jessica Im <me@jessicaim.com>
2021-11-02 17:40:42 -0400

    saving in working put route

--- recipe-front-end/src/components/create.js
+++ recipe-front-end/src/components/create.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Create = (props) => {
-     const [recipe,setRecipes]=useState()
+     const [recipes,setRecipes]=useState([])
      const [newName,setName]=useState()
      const [newDescription,setDescription]=useState()
      const [newServings,setServings]=useState()
@@ -49,14 +49,14 @@ const Create = (props) => {
           axios.post(
                'http://localhost:3001/recipes',
                {
-                    // name: name,
-                    // description: description,
-                    // servings: servings,
-                    // time: time,
-                    // ingredients: ingredients,
-                    // preparation: preparation,
-                    // image: image,
-                    // tags: tags
+                    name: newName,
+                    description: newDescription,
+                    servings: newServings,
+                    time: newTime,
+                    ingredients: newIngredients,
+                    preparation: newPrep,
+                    image: newImg,
+                    tags: newTags
                }
           ).then(() => {
                axios
@@ -77,20 +77,37 @@ const Create = (props) => {
 
 
      return (
-          <section>
-               <h2>Add a Recipe!</h2>
-               <form onSubmit={handleFormSubmit}>
-                    Recipe name: <input type="text" onChange={handleName}/><br/>
-                    Description: <input type="text" onChange={handleDescription}/><br/>
-                    Servings: <input type="text" onChange={handleServings} placeholder="how many people does this serve?"/><br/>
-                    Time: <input type="text" onChange={handleTime} placeholder="how long does it take to make? Ex. 25 minutes, 1 hour"/><br/>
-                    Ingredients: <input type="text" onChange={handleIngredients}/><br/>
-                    Preparation: <input type="text" onChange={handlePreparation}/><br/>
-                    Image: <input type="text" onChange={handleImage}/><br/>
-                    Tags: <input type="text" onChange={handleTags}/><br/>
-                    <input type="submit" value="Add Recipe"/>
-               </form>
-          </section>
+          <main>
+               <section>
+                    <h2>Add a Recipe!</h2>
+                    <form onSubmit={handleFormSubmit}>
+                         Recipe name: <input type="text" onChange={handleName}/><br/>
+                         Description: <input type="text" onChange={handleDescription}/><br/>
+                         Servings: <input type="text" onChange={handleServings} placeholder="how many people does this serve?"/><br/>
+                         Time: <input type="text" onChange={handleTime} placeholder="how long does it take to make? Ex. 25 minutes, 1 hour"/><br/>
+                         Ingredients: <input type="text" onChange={handleIngredients}/><br/>
+                         Preparation: <input type="text" onChange={handlePreparation}/><br/>
+                         Image: <input type="text" onChange={handleImage}/><br/>
+                         Tags: <input type="text" onChange={handleTags}/><br/>
+                         <input type="submit" value="Add Recipe"/>
+                    </form>
+               </section>
+               <section>
+                    <h2>Recipes</h2>
+                    <ul>
+                         {
+                              recipes.map((recipe) => {
+                                   return <li key={recipe._id}>
+                                        {
+                                             recipe.name,
+                                             recipe.description
+                                        }
+                                   </li>
+                              })
+                         }
+                    </ul>
+               </section>
+          </main>
      )
 }
 
